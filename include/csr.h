2aa9a7b42b5c2d483ca901669491230fd299268b
Henrik Karlsson <hakarlsson@mailbox.org>
2022-02-17 16:20:37 +0100

    Adds more CSR operations.

--- include/csr.h
+++ include/csr.h
@@ -3,8 +3,16 @@
 #pragma once
 
 #define read_csr(reg) ({ \
-                register unsigned long _val; \
-                __asm__("csrr %0,"#reg:"=r"(_val)); \
-                _val; \
+                register unsigned long _out; \
+                __asm__("csrr %0,"#reg:"=r"(_out)); \
+                _out; \
         })
 
+#define write_csr(reg, _in) \
+                __asm__("csrw "#reg",%0"::"r"(_in))
+
+#define swap_csr(reg, _in) ({ \
+                register unsigned long _out; \
+                __asm__("csrr %0,"#reg",%1":"=r"(_out):"r"(_in)); \
+                _out; \
+        })
