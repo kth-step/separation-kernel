e289d88432151cdbfe42e65fa244389d7819e6db
Henrik Karlsson <hakarlsson@mailbox.org>
2022-03-20 17:40:11 +0100

    Removes lock from user.c.

--- src/user.c
+++ src/user.c
@@ -3,10 +3,7 @@
 #include "lock.h"
 
 void main(uintptr_t pid, uint64_t begin, uint64_t end) {
-        static Lock l;
         while (1) {
-        acquire_lock(&l);
-        printf("%llu\t%llx\t%llx\r\n", pid, begin, end);
-        release_lock(&l);
+                printf("%llu\t%llx\t%llx\r\n", pid, begin, end);
         }
 }
