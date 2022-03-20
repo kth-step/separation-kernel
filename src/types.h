b0351091f6306e4022a8e4a9d7225834d46d18bf
Henrik Karlsson <hakarlsson@mailbox.org>
2022-03-20 17:45:03 +0100

    Replace types with stdint defined types.

--- src/types.h
+++ src/types.h
@@ -1,8 +1,3 @@
 // See LICENSE file for copyright and license details.
 #pragma once
-
-typedef unsigned long long uintptr_t;
-typedef unsigned char uint8_t;
-typedef unsigned long long uint64_t;
-typedef unsigned long size_t;
-
+#include <stdint.h>
