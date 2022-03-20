9897815d1d10ecb825763bf125f01a4db3b8de3e
Henrik Karlsson <hakarlsson@mailbox.org>
2022-03-20 17:29:09 +0100

    Optimizes lock.

--- src/lock.h
+++ src/lock.h
@@ -1,35 +1,32 @@
 // See LICENSE file for copyright and license details.
 #pragma once
+#include <stdint.h>
 
-typedef volatile int Lock;
+typedef unsigned long Lock;
 
-inline void acquire_lock(Lock *lock) {
-        asm volatile (
-                "li t1,1\n"
-                "1:lr.w.aq t2,0(%0)\n" 
-                "bnez t2,1b\n"
-                "sc.w t2,t1,0(%0)\n"
-                "bnez t2,1b\n"
-                :: "r"(lock) : "t2","t1", "memory");
-}
-
-// Returns zero success, non-zero on fail.
-inline int try_acquire_lock(Lock *lock) {
-        int t0;
-        asm volatile (
-                "li t0,1\n"
-                "1:lr.w.aq %0,0(%1)\n" 
-                "bnez %0,2f\n"
-                "sc.w %0,t0,0(%1)\n"
-                "bnez %0,1b\n"
-                "2:"
-                : "=r"(t0) : "r"(lock) : "t0", "memory");
+static inline
+uintptr_t try_acquire_lock(Lock *l) {
+        register uintptr_t t0;
+        __asm__ volatile (
+                "1:lr.d.aq %0,(%1)\n"
+                "  bnez %0,2f\n"
+                "  sc.d %0,%1,(%1)\n"
+                "  bnez %0,1b\n"
+                "2:\n" :"=r&"(t0):"r"(l):"memory");
         return t0;
 }
 
-inline void release_lock(Lock *lock) {
-        asm volatile (
-                "fence rw,w\n"
-                "sw x0,0(%0)"
-                :: "r"(lock) : "memory");
+static inline
+void acquire_lock(Lock *l) {
+        __asm__ volatile (
+                "1:lr.d.aq t0,(%0)\n"
+                "  bnez t0,1b\n"
+                "  sc.d t0,%0,(%0)\n"
+                "  bnez t0,1b\n"
+                ::"r"(l):"t0","memory");
+}
+
+static inline
+void release_lock(Lock *l) {
+        __asm__ volatile ("amoswap.d.rl zero,zero,(%0)"::"r"(l):"memory");
 }
