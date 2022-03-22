eb33d15fbdd53754276667731cc33b923e855d0b
Henrik Karlsson <hakarlsson@mailbox.org>
2022-03-22 16:30:12 +0100

    Comment proc.h, proc.c.

--- src/proc.c
+++ src/proc.c
@@ -3,18 +3,44 @@
 #include "proc.h"
 #include "stack.h"
 
+/** Initial stack offset.
+ * The initialization of a process is a bit awkward, we basically
+ * start in the middle of a function call, for this we have this
+ * stack offset which should be safe. It must fit all registers saved
+ * to the stack in switch.S and entry.S.
+ */
+#define INIT_STACK_OFFSET 32
+
+/** AsmTrapExit
+ * This assembly function goes to user mode.
+ */
 extern void AsmTrapExit();
+
+/** User Code
+ * This is a temporary function for development. It points to the
+ * user code we use for development.
+ */
 extern void user_code();
 
+/* Defined in proc.h */
 Process processes[N_PROC];
 
-#define INIT_STACK_OFFSET 32
-void InitProc(void) {
-        for (int i = 0; i < N_PROC; i++) {
-                Process *proc = &processes[i];
-                proc->pid = i;
-                proc->ksp = &proc_stack[i][STACK_SIZE/8-INIT_STACK_OFFSET];
-                *proc->ksp = (uintptr_t)AsmTrapExit;
-                proc->pc = (uintptr_t*)user_code;
-        }
+/* Initializes one process. */
+static void init_proc(int pid) {
+        /* Get the PCB */
+        Process *proc = processes + pid;
+        /* Set the process id to */
+        proc->pid = pid;
+        /* Set the process's kernel stack. */
+        proc->ksp = &proc_stack[pid][STACK_SIZE/8-INIT_STACK_OFFSET];
+        /* Set the return address located as first element on the stack. */
+        *proc->ksp = (uintptr_t)AsmTrapExit;
+        /* Set the user program counter, this will be the entry point. */
+        proc->pc = (uintptr_t*)user_code;
+}
+
+/* Defined in proc.h */
+void InitProcesses(void) {
+        for (int i = 0; i < N_PROC; i++)
+                init_proc(i);
 }
--- src/proc.h
+++ src/proc.h
@@ -6,14 +6,61 @@
 #include "capabilities.h"
 #include "lock.h"
 
+/** Process
+ * This is the Process Control Block (PCB). We store pointers
+ * register and process's state in this struct.
+ */
 typedef struct process {
+        /** Kernel stack pointer. 
+         *
+         * When a process is running, we store the core's stack pointer 
+         * (pointer to core_stack) to ksp.
+         *
+         * When the process is not running, i.e., when the scheduler or some 
+         * other process is running, then we store the process's stack pointer 
+         * (pointer to proc_stack) to ksp.
+         */
         uintptr_t *ksp;
+        /** Process identifier.
+         * This is the process's ID used for identification during 
+         * inter-process communication.
+         */
         uintptr_t pid;
-        Capability **capabilities;
+        /** Capability table. 
+         * Pointer to the capability table.
+         */
+        Capability **cap_table;
+        /** Argument registers.
+         * We store the argument registers a0-a7 in the args array,
+         * this simplifies inter-process communication.
+         */
         uintptr_t args[8];
+        /** Program counter.
+         * We store the process's program counter here, this simplifies
+         * exception handling and supervisor capabilities.
+         */
         uintptr_t *pc;
+        /** Lock.
+         * A core has to claim this lock before running the process.
+         * The lock value is zero when unclaimed, non-zero when claimed.
+         */
         uintptr_t lock;
 } Process;
 
+/** Processes
+ * We have a static number of processes N_PROC. The initial process is always
+ * process 0.
+ */
 extern Process processes[N_PROC];
+
+/** Current process
+ * This is a core local variable. If the core has a process, then current
+ * points to the claimed process, otherwise it is NULL.
+ */
 register Process *current __asm__("tp");
+
+/** Initialize process.
+ * This initializes all processes, setting their PID, stack pointer, and 
+ * entry points.
+ */
+void InitProcesses(void);
