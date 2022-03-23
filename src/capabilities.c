e70637fac07571757073c1ea3d43f1233bb7c3fe
Henrik Karlsson <hakarlsson@mailbox.org>
2022-03-23 11:51:36 +0100

    Fixes capability is_child for memory

--- src/capabilities.c
+++ src/capabilities.c
@@ -10,7 +10,7 @@ static inline int CapIsChildTsTs(TimeSlice *parent, TimeSlice *child) {
                (child->tsid < parent->tsid + parent->fuel);
 }
 
-static inline int BoundsOfPe(PmpEntry *pe, uintptr_t *begin, uintptr_t *end) {
+static inline int bound_of_pe(PmpEntry *pe, uintptr_t *begin, uintptr_t *end) {
         int A = (pe->cfg >> 3) & 0x3;
         switch (A) {
                 case 1:
@@ -20,16 +20,25 @@ static inline int BoundsOfPe(PmpEntry *pe, uintptr_t *begin, uintptr_t *end) {
                 case 2:
                         *begin = pe->addr;
                         *end = pe->addr + 4;
+                        break;
                 case 3:
                         *begin = (pe->addr & (pe->addr + 1));
+                        *end = *begin + (pe->addr ^ (pe->addr + 1)) + 1;
+                        break;
                 default:
                         break;
         }
         return A;
 }
 
-static inline int CapIsChildMsPe(MemorySlice *parent, PmpEntry *child) {
-        return 0;
+static inline int is_child_ms_pe(MemorySlice *parent, PmpEntry *child) {
+        uintptr_t pe_begin, pe_end;
+        bound_of_pe(child, &pe_begin, &pe_end);
+        return parent->begin <= pe_begin && pe_end <= parent->end;
+}
+
+static inline int is_child_ms_ms(MemorySlice *parent, MemorySlice *child) {
+        return parent->begin <= child->begin && child->end <= parent->end;
 }
 
 static inline int CapIsChild(Capability *parent, Capability *child) {
@@ -38,7 +47,10 @@ static inline int CapIsChild(Capability *parent, Capability *child) {
                 return CapIsChildTsTs(&parent->time_slice, &child->time_slice);
         } else if (cap_is_type(parent, CAP_MEMORY_SLICE) &&
                    cap_is_type(child, CAP_PMP_ENTRY)) {
-                return CapIsChildMsPe(&parent->memory_slice, &child->pmp_entry);
+                return is_child_ms_pe(&parent->memory_slice, &child->pmp_entry);
+        } else if (cap_is_type(parent, CAP_MEMORY_SLICE) &&
+                   cap_is_type(child, CAP_MEMORY_SLICE)) {
+                return is_child_ms_ms(&parent->memory_slice, &child->memory_slice);
         }
         return 0;
 }
