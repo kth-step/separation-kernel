e8129d2353ef808cf692bcd5ad5f03750778b62c
Henrik Kultala <kultala@kth.se>
2022-06-13 14:48:32 +0200

    Remove a compiler warning

--- crypto-app/wolfssl/wolfssl/wolfcrypt/settings.h
+++ crypto-app/wolfssl/wolfssl/wolfcrypt/settings.h
@@ -2383,7 +2383,7 @@ extern void uITRON4_free(void *p) ;
 #endif
 
 /* warning for not using harden build options (default with ./configure) */
-#ifndef WC_NO_HARDEN
+/*#ifndef WC_NO_HARDEN
     #if (defined(USE_FAST_MATH) && !defined(TFM_TIMING_RESISTANT)) || \
         (defined(HAVE_ECC) && !defined(ECC_TIMING_RESISTANT)) || \
         (!defined(NO_RSA) && !defined(WC_RSA_BLINDING) && !defined(HAVE_FIPS) && \
@@ -2395,7 +2395,7 @@ extern void uITRON4_free(void *p) ;
             #pragma message("Warning: For timing resistance / side-channel attack prevention consider using harden options")
         #endif
     #endif
-#endif
+#endif*/
 
 #ifdef OPENSSL_COEXIST
     /* make sure old names are disabled */
