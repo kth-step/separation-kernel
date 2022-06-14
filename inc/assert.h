5683af8912876974837fe0ef3f008d8fee49e4f3
Henrik Karlsson <hakarlsson@mailbox.org>
2022-06-14 13:08:30 +0200

    Better assert feedback

--- inc/assert.h
+++ inc/assert.h
@@ -2,14 +2,12 @@
 #ifndef NDEBUG
 #include <stdio.h>
 
-#define ASSERT(val)                                                         \
-        ({                                                                  \
-                if (!(val)) {                                               \
-                        printf("Assert violated in function %s (%s:%d).\n", \
-                               __FUNCTION__, __FILE__, __LINE__);           \
-                        while (1)                                           \
-                                ;                                           \
-                }                                                           \
+void _assert_fail(const char *, const char *, unsigned int, const char *) __attribute__((noreturn));
+
+#define ASSERT(val)                                                           \
+        ({                                                                    \
+                if (!(val))                                                   \
+                        _assert_fail(#val, __FILE__, __LINE__, __FUNCTION__); \
         })
 #else
 #define ASSERT(val)
