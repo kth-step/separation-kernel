605f773190fd184fcbc718cf63d69a1aef7f6ca4
Henrik Kultala <kultala@kth.se>
2022-07-05 14:46:53 +0200

    Update SchedUpdatePidTsid to match SchedUpdate

--- src/sched.h
+++ src/sched.h
@@ -49,14 +49,16 @@ static inline void SchedUpdatePidTsid(uint8_t begin, uint8_t end,
                                       uint8_t hartid, uint8_t pid_expected,
                                       uint8_t tsid_expected,
                                       uint8_t pid_desired,
-                                      uint8_t tsid_desired);
+                                      uint8_t tsid_desired, 
+                                      Cap * c);
 
 void SchedUpdatePidTsid(uint8_t begin, uint8_t end, uint8_t hartid,
                         uint8_t pid_expected, uint8_t tsid_expected,
-                        uint8_t pid_desired, uint8_t tsid_desired) {
+                        uint8_t pid_desired, uint8_t tsid_desired, 
+                        Cap * c) {
         uint16_t expected = (tsid_expected << 8) | pid_expected;
         uint16_t desired = (tsid_desired << 8) | pid_desired;
-        SchedUpdate(begin, end, hartid, expected, desired);
+        SchedUpdate(begin, end, hartid, expected, desired, c);
 }
 
 void InitSched();
