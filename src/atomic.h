2664d1e8df3c87da86bf38dc15929e9a494d37b4
Henrik Karlsson <hakarlsson@mailbox.org>
2022-08-04 10:06:54 +0200

    Improved atomics

--- src/atomic.h
+++ src/atomic.h
@@ -3,11 +3,45 @@
 #include <stdint.h>
 
 #define synchronize() __sync_synchronize()
-#define compare_and_set(ptr, expected, desired) __sync_bool_compare_and_swap(ptr, expected, desired)
-#define compare_and_swap(ptr, expected, desired) __sync_val_compare_and_swap(ptr, expected, desired)
 #define fetch_and_and(ptr, val) __sync_fetch_and_and(ptr, val)
 #define fetch_and_or(ptr, val) __sync_fetch_and_or(ptr, val)
 #define fetch_and_add(ptr, val) __sync_fetch_and_add(ptr, val)
+
+#ifndef BUILTIN_ATOMIC
+// the builtin atomic compare_and_swap/set always uses registers a0 and a1 as temporary registers, selects the temporary
+// registers dynamically, reducing the amount of stack usage.
+#define compare_and_swap(ptr, expected, desired)                      \
+        ({                                                            \
+                register uint64_t tmp0, tmp1;                         \
+                if (sizeof(*ptr) == 8) {                              \
+                        __asm__ volatile(                             \
+                            "1:lr.d.aq %0,(%2)\n"                     \
+                            "bne    %0,%3,2f\n"                       \
+                            "sc.d %1,%4,(%2)\n"                       \
+                            "bnez   %1,1b\n"                          \
+                            "2:"                                      \
+                            : "=&r"(tmp0), "=&r"(tmp1)                \
+                            : "r"(ptr), "r"(expected), "r"(desired)); \
+                } else if (sizeof(*ptr) == 4) {                       \
+                        __asm__ volatile(                             \
+                            "1:lr.w.aq %0,(%2)\n"                     \
+                            "bne    %0,%3,2f\n"                       \
+                            "sc.w.rl %1,%4,(%2)\n"                    \
+                            "bnez   %1,1b\n"                          \
+                            "2:"                                      \
+                            : "=&r"(tmp0), "=&r"(tmp1)                \
+                            : "r"(ptr), "r"(expected), "r"(desired)); \
+                } else {                                              \
+                        while (1)                                     \
+                                ;                                     \
+                }                                                     \
+                (typeof(*ptr))tmp0;                                   \
+        })
+#define compare_and_set(ptr, expected, desired) (compare_and_swap(ptr, expected, desired) == expected)
+#else
+#define compare_and_set(ptr, expected, desired) __sync_bool_compare_and_swap(ptr, expected, desired)
+#define compare_and_swap(ptr, expected, desired) __sync_val_compare_and_swap(ptr, expected, desired)
+#endif
 
 typedef struct marked_pointer {
         uint64_t data;
